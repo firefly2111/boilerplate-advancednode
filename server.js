f24ed846fd19405815c96dd1b9be19297eedfd5f
Nikola Milicevic <nikolamilicevic99@yahoo.com>
2019-04-28 12:28:19 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,62 +3,57 @@
 const express     = require('express');
 const bodyParser  = require('body-parser');
 const fccTesting  = require('./freeCodeCamp/fcctesting.js');
-const passport    = require("passport");
-const session     = require("express-session");
-const app         = express();
-const ObjectID    = require("mongodb").ObjectID;
-const mongo       = require("mongodb").MongoClient;
+const passport = require("passport");
+const session = require("express-session");
+const ObjectID = require("mongodb").ObjectID;
+const mongo = require("mongodb").MongoClient;
 const LocalStrategy = require("passport-local");
+
+const app = express();
 
 fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "pug");
-app.use(session({
-    secret: process.env.SESSION_SECRET,
-    resave: true,
-    saveUnitialized: true
-}));
 app.use(passport.initialize());
-app.use(passport.session());
-
+app.use(passport.session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUnitialized: true
+}));
 
 mongo.connect(process.env.DATABASE, (err, db) => {
   if(err){
-    return err;
+    console.log(err);
   }else{
     passport.serializeUser((user, done) => {
       done(null, user._id);
     });
     passport.deserializeUser((id, done) => {
-      db.collection("users").findOne({_id: new ObjectID(id)}, (err, doc) => {done(null, doc)});
+      db.collection("users").findOne({_id: new ObjectID(id)}, (err, doc) => {
+        
+      });
     });
-  
     passport.use(new LocalStrategy((username, password, done) => {
       db.collection("users").findOne({username: username}, (err, user) => {
-        if(err){
-          return done(err);
-        }
-        if(!user){
-          return done(null, false);
-        }
-        if(password !== user.password){
-          return done(null, false);  
-        }
+        if(err){return done(err)}
+        if(!user){return done(null, false)}
+        if(password !== user.password){return done(null, false)}
         return done(null, user);
       });
     }));
     
+    
     app.listen(process.env.PORT || 3000, () => {
-      console.log("Listening mongo");
+      console.log("Listening on port " + process.env.PORT);
     });
   }
 });
 
 
-
 app.route('/')
   .get((req, res) => {
-    res.render(process.cwd() + '/views/pug/index.pug', {title: "Hello", message: "Please login", showLogin: true});
+    res.render(process.cwd() + '/views/pug/index.pug', {title: "Hello", message: "Please login56", showLogin: true});
   });
+
