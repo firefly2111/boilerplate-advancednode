7b3c2a2c3923ff2568c9f9de4f822d2dce38b8b1
Nikola Milicevic <nikolamilicevic99@yahoo.com>
2019-04-29 19:35:17 +0200

    Update server.js

--- server.js
+++ server.js
@@ -59,7 +59,28 @@ mongo.connect(process.env.DATABASE, (err, db) => {
       res.render(process.cwd() + '/views/pug/profile', {username: req.user.username});
       });
     
-    
+    app.route('/register')
+          .post((req, res, next) => {
+              db.collection('users').findOne({ username: req.body.username }, function (err, user) {
+                  if(err) {
+                      next(err);
+                  } else if (user) {
+                      res.redirect('/');
+                  } else {
+                      db.collection('users').insertOne({username: req.body.username, password: req.body.password}, (err, doc) => {
+                            if(err) {
+                                res.redirect('/');
+                            } else {
+                                next(null, user);
+                            }
+                        }
+                      )
+                  }
+              })},
+            passport.authenticate('local', { failureRedirect: '/' }), (req, res, next) => {
+                res.redirect('/profile');
+            }
+        );
     
     
     app.route("/logout").get((req,res) => {
