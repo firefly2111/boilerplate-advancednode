9da52b9cace385c930ad99a2237a1d65d1a9029a
Nikola Milicevic <nikolamilicevic99@yahoo.com>
2019-05-02 17:00:32 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,34 +1,74 @@
 'use strict';
 
 const express     = require('express');
+const session     = require('express-session');
 const bodyParser  = require('body-parser');
 const fccTesting  = require('./freeCodeCamp/fcctesting.js');
-const session     = require('express-session');
-const mongo       = require('mongodb').MongoClient;
-const routes      = require('./routes.js');
 const auth        = require('./auth.js');
+const routes      = require('./routes.js');
+const mongo       = require('mongodb').MongoClient;
+const passport    = require('passport');
+const cookieParser= require('cookie-parser')
+const app         = express();
+const http        = require('http').Server(app);
+const sessionStore= new session.MemoryStore();
+const io = require("socket.io")(http);
+const cors = require("cors");
+const passportSocketIo = require("passport.socketio");
 
-const app = express();
+app.use(cors());
 
 fccTesting(app); //For FCC testing purposes
 
 app.use('/public', express.static(process.cwd() + '/public'));
+app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.set('view engine', 'pug')
 
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  key: 'express.sid',
+  store: sessionStore,
+}));
+
+
 mongo.connect(process.env.DATABASE, (err, db) => {
-    if(err) {
-        console.log('Database error: ' + err);
-    } else {
-        console.log('Successful database connection');
-
-        auth(app, db);
+    if(err) console.log('Database error: ' + err);
+  
+    auth(app, db);
+    routes(app, db);
       
-        routes(app, db);
+    http.listen(process.env.PORT || 3000);
 
-        app.listen(process.env.PORT || 3000, () => {
-          console.log("Listening on port " + process.env.PORT);
-        });  
-}});
+  
+    //start socket.io code  
+  
+    io.use(passportSocketIo.authorize({
+      cookieParser: cookieParser,
+      key: "express.sid",
+      secret: process.env.SESSION_SECRET,
+      store: sessionStore
+    }));
+  
+    var currentUsers = 0;
+    io.on("connection", socket => {
+      console.log("A user has Connected");
+      currentUsers++;
+      io.emit("user count", currentUsers);
+      io.emit("user", {name: socket.request.user.name, currentUsers, connected: true});
+      socket.on("chat message", (message) => {
+        io.emit("chat message", {name: socket.request.user.name, message});
+      });
+      socket.on("disconnect", () => {
+        currentUsers--;
+      });
+    });
+  
+
+    //end socket.io code
+  
+  
+});
