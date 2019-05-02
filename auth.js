bc04c866b402c7dc1658006a9b803d51a1cef753
Nikola Milicevic <nikolamilicevic99@yahoo.com>
2019-05-02 17:03:27 +0200

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,43 +1,54 @@
 const session     = require('express-session');
+const mongo       = require('mongodb').MongoClient;
 const passport    = require('passport');
-const ObjectID    = require('mongodb').ObjectID;
-const LocalStrategy = require('passport-local');
-const bcrypt      = require('bcrypt');
+const GitHubStrategy = require('passport-github').Strategy;
 
-module.exports = function (app,db) {
+module.exports = function (app, db) {
   
-  app.use(session({
-    secret: process.env.SESSION_SECRET,
-    resave: true,
-    saveUninitialized: true,
-  }));
-  app.use(passport.initialize());
-  app.use(passport.session());
-  
-  passport.serializeUser((user, done) => {
-    done(null, user._id);
-  });
+    app.use(passport.initialize());
+    app.use(passport.session());
 
-  passport.deserializeUser((id, done) => {
-      db.collection('users').findOne(
-          {_id: new ObjectID(id)},
-          (err, doc) => {
-              done(null, doc);
-          }
-      );
-  });
+    passport.serializeUser((user, done) => {
+      done(null, user.id);
+    });
 
-  passport.use(new LocalStrategy(
-    function(username, password, done) {
-      db.collection('users').findOne({ username: username }, function (err, user) {
-        console.log('User '+ username +' attempted to log in.');
-        if (err) { return done(err); }
-        if (!user) { return done(null, false); }
-        if (!bcrypt.compareSync(password, user.password)) { return done(null, false); }
-        //if (password !== user.password) { return done(null, false); }
-        return done(null, user);
-      });
-    }
-  ));
+    passport.deserializeUser((id, done) => {
+        db.collection('chatusers').findOne(
+            {id: id},
+            (err, doc) => {
+                done(null, doc);
+            }
+        );
+    });
+
+    passport.use(new GitHubStrategy({
+        clientID: process.env.GITHUB_CLIENT_ID,
+        clientSecret: process.env.GITHUB_CLIENT_SECRET,
+        callbackURL: "https://thinkable-tub.glitch.me/auth/github/callback"
+      },
+      function(accessToken, refreshToken, profile, cb) {
+          db.collection('chatusers').findAndModify(
+              {id: profile.id},
+              {},
+              {$setOnInsert:{
+                  id: profile.id,
+                  name: profile.displayName || 'Anonymous',
+                  photo: profile.photos[0].value || '',
+                  email: profile.emails[0].value || 'No public email',
+                  created_on: new Date(),
+                  provider: profile.provider || '',
+                  chat_messages: 0
+              },$set:{
+                  last_login: new Date()
+              },$inc:{
+                  login_count: 1
+              }},
+              {upsert:true, new: true}, //Insert object if not found, Return new object after modify
+              (err, doc) => {
+                  return cb(null, doc.value);
+              }
+          );
+        }
+    ));
   
 }
